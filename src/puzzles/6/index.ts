d228eacd137550fd64ada1700a73c161fb09dab6
Aleksandras Nelkinas <a.nelkinas@gmail.com>
2023-12-07 22:27:54 +0700

    Improve day 6 code

--- src/puzzles/6/index.ts
+++ src/puzzles/6/index.ts
@@ -2,18 +2,17 @@ import _ from 'lodash';
 import { parseInput } from './utils.ts';
 
 const solve = (input: string, { joined = false } = {}) =>
-  parseInput(input, { joined }).reduce((result, [time, distance]) => {
-    const waysToWin = _(1)
-      .range(time + 1)
-      .filter((chargeTime) => {
-        const speed = chargeTime;
-        const remainingTime = time - chargeTime;
-        const travelledDistance = remainingTime * speed;
-        return travelledDistance > distance;
-      })
-      .sumBy(() => 1);
-
-    return result * waysToWin;
-  }, 1);
+  parseInput(input, { joined })
+    .map(([time, distance]) =>
+      _(1)
+        .range(time + 1)
+        .filter((chargeTime) => {
+          const speed = chargeTime;
+          const remainingTime = time - chargeTime;
+          return speed * remainingTime > distance;
+        })
+        .size(),
+    )
+    .reduce((result, waysToWin) => result * waysToWin, 1);
 
 export { solve };
